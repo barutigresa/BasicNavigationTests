39e42fdf3ed60d69354ae3bff23a558eb3b3e8f7
Gresa <gresa@cybertekschool.com>
2020-06-17 13:29:23 +0200

    Title3 HW2

---
+++ src/test/java/com/cbt/tests/TitleVerification3.java
@@ -0,0 +1,31 @@
+package com.cbt.tests;
+
+import com.cbt.utilities.BrowserFactory;
+import org.openqa.selenium.WebDriver;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class TitleVerification3 {
+    public static void main(String[] args) {
+        List<String> urls = Arrays.asList("https://luluandgeorgia.com",
+                "https://wayfair.com", "https://www.westelm.com", "https://walmart.com");
+
+        for(int i =0 ; i < urls.size(); i++)
+        {
+            WebDriver driver = BrowserFactory.getDriver("chrome");
+            driver.get(urls.get(i));
+            if(urls.get(i).toLowerCase().contains(driver.getTitle().replace(" ", "").toLowerCase()))
+            {
+                System.out.println(driver.getTitle() + " PASSED");
+            }
+            else
+            {
+                System.out.println(driver.getTitle() + " FAILED");
+                System.out.println(urls.get(i));
+            }
+            driver.close();
+        }
+
+    }
+}
